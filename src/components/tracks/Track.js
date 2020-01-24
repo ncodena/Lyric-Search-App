d5698a11a7a6f66124a4731728100fe884b3334e
Nuria <c.odena.nuria@gmail.com>
2020-01-24 14:33:50 +0100

    search bar component added

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -9,7 +9,7 @@ const Track = (props) => {
                 <div className="card-body">
                     <h5>{track.artist_name}</h5>
                     <p className="card-text">
-                        <strong><i className="fas fa-play"></i>Track</strong> : {track.track_name}
+                        <strong><i className="fas fa-play"></i>Track</strong>  : {track.track_name}
                         <br/>
                         <strong><i className="fas fa-compact-disc">Album</i></strong> : {track.album_name}
                     </p>
--- src/context.js
+++ src/context.js
@@ -3,15 +3,29 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'SEARCH_TRACKS':
+        return{
+            ...state,
+            track_list: action.payload,
+            heading:'Search Results',
+        };
+        default:
+            return state
+    }
+};
+
 export class Provider extends Component {
     state = {
         track_list: [],
-        heading: 'Top 10 Tracks'
+        heading: 'Top 100 Tracks',
+        dispatch: action => this.setState(state => reducer(state, action))
     };
 
     componentDidMount(){
         axios
-        .get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+        .get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=100&country=us&f_has_lyrics=1&apikey=${
             process.env.REACT_APP_MM_KEY}`)
         .then(res => {
             // console.log(res.data);
