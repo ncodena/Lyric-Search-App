549213ad2a02fe8bf3d8898af6e78df7be2c2a2f
Nuria <c.odena.nuria@gmail.com>
2020-01-24 16:46:43 +0100

    axios call to album.get and new data displayed

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -23,21 +23,31 @@ class Lyrics extends Component {
                 process.env.REACT_APP_MM_KEY}`)
         })
         .then(res => {
-            // console.log(res.data)
+            console.log(res.data)
             this.setState({track: res.data.message.body.track});
-            return axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/album.tracks.get?track_id=${this.props.match.params.id}&apikey=${
+            let album_id = res.data.message.body.track.album_id;
+            return axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/album.get?album_id=${album_id}&apikey=${
                 process.env.REACT_APP_MM_KEY}`)
         })
         .then(res => {
             console.log(res.data)
-            // this.setState({album: res.data.message.body.track_list});
+            this.setState({album: res.data.message.body.album});
+            let album_id = res.data.message.body.album.album_id;
+            return axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/album.tracks.get?album_id=${album_id}&apikey=${
+                process.env.REACT_APP_MM_KEY}`)
+        })
+        .then(res => {
+            console.log(res.data)
+            // this.setState({track: res.data.message.body.track});
         })
         .catch(err => console.log(err));
 
     }
     render() {
-        const {track, lyrics} = this.state;
+        const {track, lyrics, album} = this.state;
         console.log(track)
+        console.log(album)
+
 
         if(track === undefined || lyrics === undefined || Object.keys(track).length === 0 || Object.keys(lyrics).length === 0){
 
@@ -58,7 +68,7 @@ class Lyrics extends Component {
                     </div>
                     <ul className="list-group mt-3">
                         <li className="list-group-item">
-                            <strong>Album ID</strong>: {' ' + track.album_id}
+                            <strong>Album Name</strong>: {' ' + album.album_name}
                         </li>
                         <li className="list-group-item">
                             <strong>Song Genre</strong>: {' ' + track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
@@ -68,6 +78,12 @@ class Lyrics extends Component {
                         </li>
                         <li className="list-group-item">
                         <strong>Num. Favourite</strong>:{' ' + track.num_favourite}
+                        </li>
+                        <li className="list-group-item">
+                        <strong>Release Date</strong>:{' ' + album.album_release_date}
+                        </li>
+                        <li className="list-group-item">
+                        <strong>Album Rating</strong>:{' ' + album.album_rating}
                         </li>
                     </ul>
 
